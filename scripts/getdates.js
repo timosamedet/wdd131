77c98a1a558817399cd20dfeed75b216e8e0ea1c
timosamedet <timosamedet@yahoo.com>
2025-07-19 22:38:09 +0100

    developed  WDd 131 home page

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+const currentYear = document.getElementById("currentyear")
+const lastModified = document.getElementById("lastModified");
+
+const today = new Date();
+currentYear.innerHTML = today.getFullYear();
+lastModified.innerHTML = document.lastModified;
+

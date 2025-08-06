e59b0ee5587ad6777ac9dd9d4c6c267ecac68ab4
timosamedet <timosamedet@yahoo.com>
2025-08-06 15:23:00 +0100

    completed site plan website

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,6 @@
+const currentYear = document.getElementById("currentyear")
+const lastModified = document.getElementById("lastModified");
+
+const today = new Date();
+currentYear.innerHTML = today.getFullYear();
+lastModified.innerHTML = document.lastModified;
